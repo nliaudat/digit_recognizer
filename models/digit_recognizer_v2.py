4330c8b2529a61cb109c4dfffa4017e0b6bebd40
Nicolas Liaudat <nliaudat@users.noreply.github.com>
2025-10-11 17:40:13 +0200

    do not loose it

--- models/digit_recognizer_v2.py
+++ models/digit_recognizer_v2.py
@@ -4,60 +4,49 @@ import parameters as params
 
 def create_digit_recognizer_v2():
     """
-    Model specifically designed for smooth INT8 quantization
-    - No BatchNormalization
-    - All activations compatible with quantization
-    - Symmetric weight distributions
-    """
-    model = tf.keras.Sequential([
-        tf.keras.layers.Input(shape=params.INPUT_SHAPE),
-        
-        # First conv with explicit activation
-        tf.keras.layers.Conv2D(
-            32, (3, 3), padding='same',
-            kernel_initializer='he_normal',
-            use_bias=True,  # Important for quantization
-            name='conv1'
-        ),
-        tf.keras.layers.ReLU(name='relu1'),
-        tf.keras.layers.MaxPooling2D((2, 2), name='pool1'),
-        
-        # Second conv
-        # tf.keras.layers.Conv2D(
-            # 64, (3, 3), padding='same',
-            # kernel_initializer='he_normal',
-            # use_bias=True,
-            # name='conv2'
-        # ),
-        tf.keras.layers.DepthwiseConv2D(
-            (3, 3), padding='same',
-            depthwise_initializer='he_normal',
-            use_bias=True,
-            name='depthwise0'
-        ),
-        tf.keras.layers.ReLU(name='relu2'),
-        tf.keras.layers.MaxPooling2D((2, 2), name='pool2'),
-        
-        
-        # Third conv - depthwise for efficiency
-        tf.keras.layers.DepthwiseConv2D(
-            (3, 3), padding='same',
-            depthwise_initializer='he_normal',
-            use_bias=True,
-            name='depthwise1'
-        ),
-        tf.keras.layers.ReLU(name='depthwise_relu1'),
-        tf.keras.layers.Conv2D(
-            64, (1, 1),
-            kernel_initializer='he_normal',
-            use_bias=True,
-            name='pointwise1'
-        ),
-        tf.keras.layers.ReLU(name='relu3'),
-        
-        # Global average pooling - quantization friendly
-        tf.keras.layers.GlobalAveragePooling2D(name='gap'),
-        tf.keras.layers.Dense(params.NB_CLASSES, activation='softmax', name='output')
-    ])
+    Based on: https://www.tensorflow.org/model_optimization/guide/quantization/training_example
     
+    Key points:
+    - Avoids ReLU6 (use ReLU + Clip instead)
+    - No BatchNorm or Flatten
+    - Uses GlobalAveragePooling for dimensionality reduction
+    - Compatible with integer quantization (int8)
+    """
+    def relu6(x, name=None):
+        # Equivalent to tf.nn.relu6(x) but ensures ops are supported everywhere
+        return tf.clip_by_value(tf.nn.relu(x), 0.0, 6.0, name=name)
+
+    inputs = tf.keras.Input(shape=params.INPUT_SHAPE, name='input')
+
+    x = tf.keras.layers.Conv2D(
+        32, (3, 3), padding='same',
+        kernel_initializer='he_normal',
+        use_bias=True,
+        name='conv1'
+    )(inputs)
+    x = relu6(x, name='relu6_1')
+    x = tf.keras.layers.MaxPooling2D((2, 2), name='pool1')(x)
+
+    x = tf.keras.layers.Conv2D(
+        64, (3, 3), padding='same',
+        kernel_initializer='he_normal',
+        use_bias=True,
+        name='conv2'
+    )(x)
+    x = relu6(x, name='relu6_2')
+    x = tf.keras.layers.MaxPooling2D((2, 2), name='pool2')(x)
+
+    x = tf.keras.layers.Conv2D(
+        64, (3, 3), padding='same',
+        kernel_initializer='he_normal',
+        use_bias=True,
+        name='conv3'
+    )(x)
+    x = relu6(x, name='relu6_3')
+
+    x = tf.keras.layers.GlobalAveragePooling2D(name='global_avg_pool')(x)
+    outputs = tf.keras.layers.Dense(params.NB_CLASSES, activation='softmax', name='output')(x)
+
+    model = tf.keras.Model(inputs, outputs, name="digit_recognizer_v2")
+
     return model
