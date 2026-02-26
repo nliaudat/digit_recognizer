aa2cd7be7ff988abfb51d9dd354049bec12015c2
Nicolas Liaudat <nliaudat@users.noreply.github.com>
2026-02-26 21:53:05 +0100

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -32,8 +32,9 @@ from .digit_recognizer_v9 import create_digit_recognizer_v9
 from .digit_recognizer_v10 import create_digit_recognizer_v10
 from .digit_recognizer_v11 import create_digit_recognizer_v11
 from .digit_recognizer_v12 import create_digit_recognizer_v12
-from .digit_recognizer_v13 import create_digit_recognizer_v13
-from .digit_recognizer_v14 import create_digit_recognizer_v14
+from .digit_recognizer_v15 import create_digit_recognizer_v15
+from .digit_recognizer_v16 import create_digit_recognizer_v16
+from .digit_recognizer_v17 import create_digit_recognizer_v17
 from .esp_quantization_ready import create_esp_quantization_ready
 from .mnist_quantization import create_mnist_quantization
 from .high_accuracy_validator import create_high_accuracy_validator
@@ -67,8 +68,9 @@ __all__ = [
     'create_digit_recognizer_v10',
     'create_digit_recognizer_v11',
     'create_digit_recognizer_v12',
-    'create_digit_recognizer_v13',
-    'create_digit_recognizer_v14',
+    'create_digit_recognizer_v15',
+    'create_digit_recognizer_v16',
+    'create_digit_recognizer_v17',
     'create_esp_quantization_ready',
     'create_mnist_quantization',
     'create_high_accuracy_validator',
